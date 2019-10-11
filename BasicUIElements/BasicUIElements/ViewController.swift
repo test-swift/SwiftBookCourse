a64773513d5be71a1ac5cecb553c00d69cbe8ada
oskulska <olgaskulska@gmail.com>
2019-10-11 20:50:26 +0200

    Basic UI elements

---
+++ BasicUIElements/BasicUIElements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicUIElements
+//
+//  Created by Olha Skulska on 2019. 10. 11..
+//  Copyright © 2019. Olha Skulska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
